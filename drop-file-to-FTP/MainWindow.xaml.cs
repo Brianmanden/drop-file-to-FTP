8d8363c4da87fe558b0ababafb3b968fbea790f4
Brian Andersen <brianmanden@gmail.com>
2022-08-20 22:23:50 +0200

    ADD get filenames from drop event

--- drop-file-to-FTP/MainWindow.xaml.cs
+++ drop-file-to-FTP/MainWindow.xaml.cs
@@ -35,17 +35,15 @@ namespace drop_file_to_FTP
 
         private void list_Drop(object sender, DragEventArgs e)
         {
-            debugTextBox.Text = "file dropped";
-        }
-
-        private void list_DragEnter(object sender, DragEventArgs e)
-        {
-            debugTextBox.Text = "drag enter started";
-        }
-
-        private void list_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            debugTextBox.Text = "list view double clicked";
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            {
+                debugTextBox.Text = "file(s) dropped" + Environment.NewLine;
+                string[] droppedFilePaths = e.Data.GetData(DataFormats.FileDrop, true) as string[];
+                foreach (var filePath in droppedFilePaths)
+                {
+                    debugTextBox.Text += filePath + Environment.NewLine;
+                }
+            }
         }
     }
 }
