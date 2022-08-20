86faed97d923297c91d863de6d3640e4af90dae9
Brian Andersen <brianmanden@gmail.com>
2022-08-20 23:04:44 +0200

    ADD FTP upload functionality

--- drop-file-to-FTP/MainWindow.xaml.cs
+++ drop-file-to-FTP/MainWindow.xaml.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Net;
 using System.Windows;
-using System.Windows.Shapes;
 
 namespace drop_file_to_FTP
 {
@@ -15,13 +16,13 @@ namespace drop_file_to_FTP
             InitializeComponent();
         }
 
+        private string credentialsFilePath = "./credentials.cfg";
+
         private void saveConfig_Click(object sender, RoutedEventArgs e)
         {
             debugTextBox.Text = "save button clicked";
 
-            string filePath = "./credentials.cfg";
-
-            if (!File.Exists(filePath))
+            if (!File.Exists(credentialsFilePath))
             {
                 string credentials =
                     hostname.Text + Environment.NewLine +
@@ -29,19 +30,41 @@ namespace drop_file_to_FTP
                     username.Text + Environment.NewLine +
                     password.Password.ToString() + Environment.NewLine;
 
-                File.WriteAllText(filePath, credentials);
+                File.WriteAllText(credentialsFilePath, credentials);
             }
         }
 
         private void list_Drop(object sender, DragEventArgs e)
         {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            if (!File.Exists(credentialsFilePath))
             {
-                debugTextBox.Text = "file(s) dropped" + Environment.NewLine;
-                string[] droppedFilePaths = e.Data.GetData(DataFormats.FileDrop, true) as string[];
-                foreach (var filePath in droppedFilePaths)
+                debugTextBox.Text = "No credentials.cfg-file found." + Environment.NewLine +
+                                    "Save credentials and try again" + Environment.NewLine;
+            }
+            else
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
                 {
-                    debugTextBox.Text += filePath + Environment.NewLine;
+                    var readCredentials = File.ReadAllText(credentialsFilePath);
+                    string[] splitCredentials = readCredentials.Split(
+                        new string[] { Environment.NewLine},
+                        StringSplitOptions.None
+                    );
+
+                    debugTextBox.Text = "File(s) dropped:" + Environment.NewLine;
+                    string[] droppedFilesPaths = e.Data.GetData(DataFormats.FileDrop, true) as string[];
+
+                    using (var client = new WebClient())
+                    {
+                        string FTPURI = splitCredentials[0] + ":" + splitCredentials[1];
+                        client.Credentials = new NetworkCredential(splitCredentials[2], splitCredentials[3]);
+                        foreach (var filePath in droppedFilesPaths)
+                        {
+                            string filename = filePath.Split("\\").Last();
+                            debugTextBox.Text += filename + Environment.NewLine;
+                            client.UploadFile("ftp://" + FTPURI + "/" + filename, WebRequestMethods.Ftp.UploadFile, filePath);
+                        }
+                    }
                 }
             }
         }
